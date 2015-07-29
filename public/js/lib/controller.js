bf2c4b39aaedad42be8efeeac483219e1f33ff0a
Azrul Sudarmin <accusudarmin@gmail.com>
2015-07-29 17:42:25 +0800

    Delete controller.js

--- public/js/lib/controller.js
+++
@@ -1,9 +0,0 @@
-var def = function ( form , target , action ) {
-    var test = new ajax(form);
-
-    test.post( action , function(cb) {
-      alert(cb);
-      // window.location.href = target;
-
-    });
-}
